85e8edd7738d1db630981e9d42e492232c17aecd
Shiho ASA <asashiho@hotmail.co.jp>
2023-07-01 13:36:39 +0900

    Update: example

--- app/frontend/Components/Examples.razor.cs
+++ app/frontend/Components/Examples.razor.cs
@@ -7,9 +7,9 @@ public sealed partial class Examples
     [Parameter, EditorRequired] public required string Message { get; set; }
     [Parameter, EditorRequired] public EventCallback<string> OnExampleClicked { get; set; }
 
-    private string WhatIsIncluded { get; } = "What is included in my Northwind Health Plus plan that is not in standard?";
-    private string WhatIsPerfReview { get; } = "What happens in a performance review?";
-    private string WhatDoesPmDo { get; } = "What does a Product Manager do?";
+    private string WhatIsIncluded { get; } = "水素ハイブリット電車とは何ですか?";
+    private string WhatIsPerfReview { get; } = "燃料電池スタックについて詳しく教えてください";
+    private string WhatDoesPmDo { get; } = "水素ハイブリッド電車の静音性について説明してください";
 
     private async Task OnClickedAsync(string exampleText)
     {
